a218c74eb189054277d3a148b3937d0aea90fc43
Hailey Montgomery <hmontgomery@ggc.edu>
2016-10-25 20:00:40 -0400

    trying to fix some errors

--- karma.conf.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine'],
-	browsers: ['Firefox', 'FirefoxDeveloper', 'FirefoxAurora', 'FirefoxNightly'],
-	
-    //...
-  });
-};
