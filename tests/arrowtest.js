eb459bc94a1186c97e9d2cfa9daeba1771982fd0
Hailey Montgomery <hmontgomery@ggc.edu>
2016-10-30 10:20:02 -0400

    adding arrowtest.js

---
+++ tests/arrowtest.js
@@ -0,0 +1,12 @@
+describe('Arrow', function()
+{
+  var x = shootingCirc.x;
+  var y = shootingCirc.y;
+
+  it("Equals to shooting circle", function() {
+      x = true;
+      y = true;
+
+      expect(x).toBe(true);
+      expect(y).toBe(true);
+});
