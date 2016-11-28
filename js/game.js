ac35c31c0492f5b1d51b0d8987c372c3b397dd04
akonee1 <akone1@ggc.edu>
2016-11-28 13:12:17 -0500

    fix error

--- js/game.js
+++ js/game.js
@@ -1,24 +1,20 @@
-//October 10, 2016
+//November 26, 2016
 //Software Development 2 GGC
 //Chemistry Game
 //The code to create the canvas is written below
 
 
+//Here we have some variables used in the game to draw objects and select images;
 var canvas = document.createElement("canvas");
 canvas.id = 'canvas';
 var ctx = canvas.getContext("2d");
 
+//This variable isHit will tell us if the arrow hit the question or not
 var isHit = false;
-
-var character2 = new Image();
-character2.src = "Images/gun.png";
-
-var rotatedCoordinates = 0;
-
-//var timer = 0;
 
 //declared and initialized a count variable for timer
 //count starts a 60 seconds and counts down
+
 var count = 60, timer = setInterval(function() {
     $("#counter").html(count--);
     if(count == 0) clearInterval(timer);
@@ -39,8 +35,6 @@ var direction = 1;
 var direction2 = 1;
 var direction3 = 1;
 var direction4 = 1;
-var TO_RADIANS = Math.PI/180;
-var theAngle = 0;
 
 //declared and intialized player score to hold an int
 //player score starts at 0
@@ -57,15 +51,18 @@ var choiceD;
 //declared score and initialized to 0
 var score = 0;
 
-//add your qestions here
+//Declared and intialized an array of questions
 var questions = new Array();
 questions[0] = 'What is the correct balanced formula for Potassium Chloride?' ;
 questions[1] = 'What is the correct balanced formula for Sodium Bromide?';
 questions[2] = 'What is the correct balanced formula for Sodium Iodide?';
 questions[3] = 'What is the correct balanced formula for Sodium Oxide?';
 questions[4] = 'What is the correct balanced formula for Potassium Oxide?';
-questions[6] = 'What is the correct balanced formula for Copper (I) chloride?';
 questions[5] = 'What is the correct balanced formula for Magnesium Oxide?';
+questions[6] = 'What is the correct balanced formula for Copper (I) chloride?';
+questions[7] = 'What is the correct balanced formula for Copper (II) chloride?';
+questions[8] = 'What is the correct balanced formula for Maganese (V) bromide?';
+questions[9] = 'What is the correct balanced formula for Ammonium Nitrate?';
 
 //Really hard problems
 questions[10] = 'What is the correct balanced formula for Sodium Peroxide?' ;
@@ -81,35 +78,18 @@ questions[17] = 'What is the correct balanced formula for Phosphorus Pentachlori
 questions[18] = 'What is the correct balanced formula for Dinitrogen Tetraoxide?';
 
 
-questions[19] = 'What is the correct balanced formula for Copper (II) chloride?';
-questions[20] = 'What is the correct balanced formula for Maganese (V) bromide?';
-questions[21] = 'What is the correct balanced formula for Ammonium Nitrate?';
-questions[22] = 'What is the correct balanced formula for Potassium Iodide?';
-questions[23] = 'What is the correct balanced formula for Magnesium Chloride?';
-questions[24] = 'What is the correct balanced formula for Aluminum Chloride?';
-questions[25] = 'What is the correct balanced formula for Sodium Sulfide?';
-questions[26] = 'What is the correct balanced formula for Calcium Sulfide?';
-questions[27] = 'What is the correct balanced formula for Sodium Nitride?';
-questions[28] = 'What is the correct balanced formula for Aluminum Nitride?';
-questions [29] = 'What is the correct balanced formula for Barium Chloride?';
-questions [30] = 'What is the correct balanced formula for Calcium Bromide?';
-questions [31] = 'What is the correct balanced formula for Potassium Hydride?';
-questions [32] = 'What is the correct balanced formula for Lithium Sulfide?';
-
-
+//declared variables for questions and set default values
 var currQuestion = 0;
 var currAnswer = 0;
 var currQa = 0;
 
-var totalQuestions = 80;
+//Declared variable for total amount of questions and set default values
+var totalQuestions = 20;
 var totalAnswers = 80;
 var totalQa = 20;
-
 var previousArrow;
 
-
-
-// questions answers here
+//Declared and intialized an answers array to hold correct answer choices
 var answers = new Array();
 answers[0] = "D" ;
 answers[1] = "C";
@@ -118,6 +98,9 @@ answers[3] = "A";
 answers[4] = "D";
 answers[5] = "C";
 answers[6] = "A";
+answers[7] = "D";
+answers[8] = "C";
+answers[9] = "B";
 
 //Answers for really hard questions
 answers[10] = "A" ;
@@ -132,24 +115,7 @@ answers[16] = "A";
 answers[17] = "D";
 answers[18] = "C";
 
-
-answers[19] = "D";
-answers[20] = "C";
-answers[21] = "B";
-answers[22] = "C";
-answers[23] = "A";
-answers[24] = "D";
-answers[25] = "B";
-answers[26] = "C";
-answers[27] = "B";
-answers[28] = "A";
-answers[29] = "B";
-answers[30] = "D";
-answers [31] = "B";
-answers [32] = "C";
-*/
-
-//multiple choices question here
+//Declared and intialized an array to display answer choices to the user
 var qa = new Array();
 qa[0] = "a= PCl b= K2Cl2 c= K2Cl d= KCl" ;
 qa[1] = "a= NBr3 b= NBr c= NaBr d= NBr2";
@@ -158,6 +124,9 @@ qa[3] = "a= Na2O b= NaO c= SO2 d= SO";
 qa[4] = "a= Ka2O b= P2O c= KaO d= K2O";
 qa[5] = "a= MnO b= MO2 c= MgO d= MgO2";
 qa[6] = "a= CuCl b= ClCu c= CoCl d= CoCl2";
+qa[7] = "a= CuCl2 b= ClCu c= CrCl2 d= KuCl";
+qa[8] = "a= MgI5 b= MnI5 c= MI5 d= MnBr";
+qa[9] = "a= NH4NO2 b= NH4NO3 c= NO3NH4 d= NH4NO";
 
 //Choices for really hard questions
 qa[10] = "a= Na2O2 b= NaO c= NaO2 d= NO2," ;
@@ -171,22 +140,6 @@ qa[15] = "a= MnO b= MO2 c= MgO d= MgO2";
 qa[16] = "a= SO3 b= SO2 c= S2O3 d= S2O2";
 qa[17] = "a= P5Cl5 b= P2Cl3 c= PBr5 d= PCl5";
 qa[18] = "a= NO4 b= N4O2 c= N2O4 d= Ni2O4";
-
-qa[19] = "a= CuCl2 b= ClCu c= CrCl2 d= KuCl";
-qa[20] = "a= MgI5 b= MnI5 c= MI5 d= MnBr";
-qa[21] = "a= NH4NO2 b= NH4NO3 c= NO3NH4 d= NH4NO";
-qa[22] = "a= IK b= PI c= KI d= IP";
-qa[23] = "a= MgCl2 b= MnCl2c= d= MgCl";
-qa[24] = "a= Al3Cl b= Cl3Al c= AlC3 d= AlCl3";
-qa[25] = "a= NaS b= Na2S c= KS d= RbS2";
-qa[26] = "a= KaS b= Ca2S c= CaS d= KaS";
-qa[27] = "a= NaN b= Na3N c= K3N d= NaNH3";
-qa[28] = "a= AlN b= AlNH3 c= NAl3 d= AN"
-qa[29] = "a= BaCl2 b= BCl2 c= BCl d= Cl2Ba";
-qa[30] = "a= CBr2 b= CaBr2 c= Br2Ca d= BrCa";
-qa[31] = "a= HK b= KH c= HCl d= HBr";
-qa[32] = "a= LiO b= LiS c= Li2S d= SLi";
-
 
 //Declared and initialized sample questions
 var question1 = "Do you like Chemistry?";
@@ -202,11 +155,9 @@ var answerB = "";
 var answerC = "";
 var answerD = "";
 
-//canvas.width=window.innerWidth - 15;
-//canvas.height=window.innerHeight - 15;
-
-canvas.width=window.innerWidth;
-canvas.height=window.innerHeight;
+//The variables canvas width and height can be used to always keep track of our drawing area
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 document.body.appendChild(canvas);
 
@@ -223,7 +174,7 @@ var groundPoint = cHeight - (cHeight/4);
 var drawnBack = false;
 var firedArrow = false;
 
-
+//This function calculates the distance between two points
 var distBetween = function(p1, p2) {
   return Math.sqrt( Math.pow((p2.x-p1.x), 2)
                   + Math.pow((p2.y-p1.y), 2) );
@@ -236,6 +187,7 @@ var isInCircle = function(mousePos) {
   else return false;
 }
 
+//This function returns the current mouse position
 function getMousePos(canvas, evt) {
   var rect = canvas.getBoundingClientRect();
   return {
@@ -249,28 +201,19 @@ var mousePos;
 var mouseDown = false;
 var mouseUp = false;
 
-
+//This function is used to calculate the coordinats of the mouse
 addEventListener("mousemove", function(evt) {
   mousePos = getMousePos(canvas, evt);
 }, false);
 
-
-
+//This function will tell us if the mouse is down in order to shoot at the questions
 addEventListener("mousedown", function(evt) {
   mousePos = getMousePos(canvas, evt);
   mouseDown = true;
   mouseUp = false;
 }, false);
 
-//We re using this functions to populate an array with questions
-//function fillQuestions(){
-
-  //  for(var i = 0; i < totalQuestions; i++){
-//       addQuestion;
-  //  }
-//}
-
-
+//The mouseUp listener will tell us that we are not shooting at the moment
 addEventListener("mouseup", function(evt) {
   mousePos = getMousePos(canvas, evt);
   mouseUp = true;
@@ -278,43 +221,9 @@ addEventListener("mouseup", function(evt) {
   rotatedCoordinates = Math.PI/2 - angleBetween(mousePos, shootingCirc);
 }, false);
 
-//function drawRotatedImage(image, x, y, angle)
-function drawRotatedImage(image, x, y, angle)
-{
-    // save the current co-ordinate system
-    // before we screw with it
-  //  var angle = Math.PI/2 - angleBetween(mousePos, shootingCirc);
-    ctx.save();
-
-    // move to the middle of where we want to draw our image
-    ctx.translate(x, y);
-
-    // rotate around that point, converting our
-    // angle from degrees to radians
-    ctx.rotate(angle * TO_RADIANS);
-
-    // draw it up and to the left by half the width
-    // and height of the image
-  ctx.drawImage(image, -(image.width/2), -(image.height/2));
-// ctx.drawImage(image, 150, 150, 80, 80);
-    // and restore the co-ords to how they were when we began
-    ctx.restore();
-}
-
+//This is the most important function of the game as it determines if a collision occurred
+//between bullet and answer object
 function collisionCheck() {
-
-//var theImage = ctx.getImageData(x, y, 25, 25);
-//var pixel = theImage.data;
-//for (var i = 0; n = pixel.length, i < n; i += 1) {
-//if (pixel[i] == 0) {
-//collision = 1;
-
-      //     }
-
-//      bubblex;
-//      bubble2x;
-//      bubble3x;
-//      bubble4x;
 
        if(currentArrowCoorX >= bubble4x && currentArrowCoorX <= bubble4x + 80){
            if(currentArrowCoorY >= 400 && currentArrowCoorY <= 480){
@@ -335,7 +244,6 @@ function collisionCheck() {
               }
          }
 
-      //   playerScore++;
        }
 
        if(currentArrowCoorX >= bubble2x && currentArrowCoorX <= bubble2x + 80){
@@ -346,7 +254,6 @@ function collisionCheck() {
               }
          }
 
-    //     playerScore++;
        }
 
        if(currentArrowCoorX >= bubble3x && currentArrowCoorX <= bubble3x + 80){
@@ -358,11 +265,10 @@ function collisionCheck() {
               }
         }
 
-    //     playerScore++;
        }
 
 
-       }
+  }
 
 //Functions check to see the timer has hit 0 and notifies the user
 function checkCount()
@@ -371,9 +277,7 @@ function checkCount()
         {
             playerChoice = "Time's up!"
         }
-
 }
-
 
 //This is the function that ends the game when all the questions are done
 function gameEnd(){
@@ -390,9 +294,8 @@ function checkAnswer(){
 
   switch (playerChoice) {
   case "A":
-      //  timer++;
+
       if(count > 0){
-            //timer = 0;
             playerChoice = "A";
 
             if(answers[currAnswer] === "A"){
@@ -411,7 +314,7 @@ function checkAnswer(){
 
       break;
   case "B":
-       // timer++;
+
         if(count > 0){
             playerChoice = "B";
             if(answers[currAnswer] === "B"){
@@ -429,9 +332,9 @@ function checkAnswer(){
 
       break;
   case "C":
-    //    timer++;
+
     if(count > 0){
-             //timer = 0;
+
              playerChoice = "C";
              if(answers[currAnswer] === "C"){
                playerScore += 1;
@@ -448,9 +351,9 @@ function checkAnswer(){
 
       break;
   case "D":
-        //timer++;
+
     if(count > 0){
-          //  timer = 0;
+
             playerChoice= "0";
 
             if(answers[currAnswer] === "D"){
@@ -467,57 +370,42 @@ function checkAnswer(){
 
       break;
   default:
-      //   playerScore = 0;
-}
 
 }
 
-function grade(){
-
-    i
-
 }
 
-
-
+//This function is Used to draw all the images, the background and the entire game
 var drawScene = function() {
-  //Increased groundPoint so arrows stick where they should in the ground
-  //var ground = groundPoint + 15;
-  //Background Image
 
   var background = new Image();
-  background.src = "Images/background2.jpg";
+  background.src = "Images/game bg.jpg";
   var bubble1 = new Image();
-  bubble1.src = "Images/bubble.png";
+  bubble1.src = "Images/new bubble.png";
   var bubble2 = new Image();
-  bubble2.src = "Images/bubble.png";
+  bubble2.src = "Images/new bubble.png";
   var bubble3 = new Image();
-  bubble3.src = "Images/bubble.png";
+  bubble3.src = "Images/new bubble.png";
   var bubble4 = new Image();
-  bubble4.src = "Images/bubble.png";
+  bubble4.src = "Images/new bubble.png";
   var character = new Image();
-  character.src = "Images/gun.png";
+  character.src = "Images/gunesdvedf.png";
 
- ctx.drawImage(background,0,0,1280,600);
- ctx.drawImage(bubble1, bubblex,200,80,80);
- ctx.drawImage(bubble2, bubble2x,100,80,80);
- ctx.drawImage(bubble3, bubble3x,300,80,80);
- ctx.drawImage(bubble4, bubble4x,400,80,80);
- //ctx.rotate(180);
- ctx.drawImage(character, 550, groundPoint - 80, 80, 80);
-// drawRotatedImage(character, 550, groundPoint - 80, angle);
-//drawRotatedImage(character2, 150, 150, theAngle);
-
+  ctx.drawImage(background,0,0,1280,600);
+  ctx.drawImage(bubble1, bubblex,200,80,80);
+  ctx.drawImage(bubble2, bubble2x,100,80,80);
+  ctx.drawImage(bubble3, bubble3x,300,80,80);
+  ctx.drawImage(bubble4, bubble4x,400,80,80);
+  ctx.drawImage(character, 550, groundPoint - 80, 80, 80);
 
 }
 
-
+//This function calculates the angle between the mouse cursor and the bullet
 var angleBetween = function(p1, p2) {
   return Math.atan2(p2.y-p1.y, p2.x-p1.x);
 }
 
 //We are using aiming coordinates and dials to indicate trajectory
-
 var getAimCoords = function(mousePos) {
 
   var angle = Math.PI/2 - angleBetween(mousePos, shootingCirc);
@@ -541,7 +429,6 @@ var getAimCoords = function(mousePos) {
 }
 
 //The shooting circle is what shows the user where the arrow is going
-
 var shootingCirc = {
  x: 600,
   y: groundPoint-30,
@@ -556,10 +443,8 @@ var drawBackCirc = {
 }
 
 //The draw circles is where the actual object gets drawn into the screen
-
 var drawCircles = function() {
   ctx.beginPath();
-//  ctx.arc(shootingCirc.x, shootingCirc.y, shootingCirc.r, 0, 2*Math.PI);
   ctx.strokeStyle = "rgba(0,0,0,0.5)";
   ctx.stroke();
   ctx.beginPath();
@@ -583,7 +468,6 @@ var isDrawnBack = function() {
 }
 
 //Information written to the client is handled by this function
-
 var writeInfo = function(mousePos) {
   ctx.font = "13px Comic Sans MS";
   ctx.textAlign = "left";
@@ -652,22 +536,16 @@ var writeInfo = function(mousePos) {
 
 }
 
-
 //Every game engine needs an update function to display to the user the coordinates
-
 var update = function() {
   isDrawnBack();
   isFiredArrow();
   if (firedArrow) {
     currArrow.fireArrow();
-
-//    currentArrowCoor = currArrow.x;
     firedArrow = false;
-    //endGame();
   }
 
   //The drawing area needs to be updated
-
   ctx.clearRect(0,0,cWidth,cHeight);
 
     //this is the code for the first bubble
@@ -701,7 +579,6 @@ var update = function() {
     }
 
     //this is the code for the fourth bubble
-
     bubble4x += direction4;
     if (bubble4x > 780){
       direction4 = -1;
@@ -710,8 +587,6 @@ var update = function() {
     if (bubble4x < 180){
       direction4 = 1;
     }
-
- //theAngle += rotatedCoordinates;
 
 if(arrows.length > 1){
   currentArrowCoorX = arrows[1].rightTipCoords.x;
@@ -723,37 +598,32 @@ collisionCheck();
 checkAnswer();
 gameEnd();
 
-
 //  music = new sound("g.mp3");
 //  music.play();
 
 }
 
 //The rendering method draws the entire game
-
 var render = function() {
-  // if(mousePos) writeInfo(mousePos);
+
   drawScene();
   drawCircles();
   for(i=0; i<arrows.length; i++) {
     arrows[i].drawArrow();
-//    currentArrowCoor = arrows[i].arrowTipCoords.x;
   }
 if(mousePos) writeInfo(mousePos);
-//currentArrowCoor = 0;
-drawCircles();
-//drawRotatedImage(character2, 150, 150, 30);
+
+  drawCircles();
 }
 
 //Here is the main function that covers all the other important functions
-
 var main = function() {
+
   update();
   render();
   requestAnimationFrame(main);
+
 }
-
-
 
 var w = window;
 requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
