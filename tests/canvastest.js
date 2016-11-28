040a2aa0c413d6314993e8636ccd152bb9f22e72
Roland Abrahantes <rabrahantes@ggc.edu>
2016-11-27 23:56:21 -0500

    Canvas Test Added

---
+++ tests/canvastest.js
@@ -0,0 +1,15 @@
+
+describe('CanvasSize', function()
+
+{
+
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+
+  it("Should equal the size of our window", function() {
+    expect(width).toBe(true);
+    expect(height).toBe(true);
+
+});
+
+});
